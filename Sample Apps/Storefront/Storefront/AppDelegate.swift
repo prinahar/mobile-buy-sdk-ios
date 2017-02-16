9e65f2fab48807b88e7c2074e67d6f3760b04424
Dima Bart <dima.bart01@gmail.com>
2017-02-16 14:11:30 -0500

    Create blank sample application called Storefront.

---
+++ Sample Apps/Storefront/Storefront/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Storefront
+//
+//  Created by Dima Bart on 2017-02-16.
+//  Copyright © 2017 Shopify Inc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    // ----------------------------------
+    //  MARK: - Application Launch -
+    //
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return true
+    }
+}
+
---
+++ Sample Apps/Storefront/Storefront/CollectionsController.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionsController.swift
+//  Storefront
+//
+//  Created by Dima Bart on 2017-02-16.
+//  Copyright © 2017 Shopify Inc. All rights reserved.
+//
+
+import UIKit
+
+class CollectionsController: UIViewController {
+
+    // ----------------------------------
+    //  MARK: - View Loading -
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
